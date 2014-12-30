bcfae6c7b4cb5de05e8304c565f59ab41dc8ba41
erickson-makotoki <a820224@gmail.com>
2014-12-31 06:33:06 +0800

    Add search field (layout)

--- app/src/main/java/net/erickson/yzucss_app/Activitys/MainActivity.java
+++ app/src/main/java/net/erickson/yzucss_app/Activitys/MainActivity.java
@@ -2,19 +2,47 @@ package net.erickson.yzucss_app.Activitys;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.navdrawer.SimpleSideDrawer;
 
 import net.erickson.yzucss_app.Fragments.MainFragment;
 import net.erickson.yzucss_app.R;
 
 public class MainActivity extends Activity{
 
+    private LinearLayout SlidingMenuContent;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MainFragment mainFragment = new MainFragment();
+
+        //準備側邊選單
+        SimpleSideDrawer mSlidingMenu = new SimpleSideDrawer(this);
+        mSlidingMenu.setLeftBehindContentView( R.layout.sliding_menu );
+
+        SlidingMenuContent = (LinearLayout) this.findViewById(R.id.sliding_menu);
+        loadList(SlidingMenuContent);
+
+        //填充Fragment
+        MainFragment mainFragment = new MainFragment(mSlidingMenu);
         getFragmentManager().beginTransaction().replace(R.id.main_layout, mainFragment).commit();
+
+
+    }
+
+
+    private void loadList(LinearLayout v)
+    {
+        Button button = new Button(this);
+        button.setText(R.string.addTableButton);
+        button.setBackground(null);
+        button.setTextColor(getResources().getColor(R.color.menuButtonText));
+        v.addView(button);
     }
 
 
--- app/src/main/java/net/erickson/yzucss_app/Fragments/MainFragment.java
+++ app/src/main/java/net/erickson/yzucss_app/Fragments/MainFragment.java
@@ -2,17 +2,59 @@ package net.erickson.yzucss_app.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.navdrawer.SimpleSideDrawer;
 
 import net.erickson.yzucss_app.R;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener {
+    private SimpleSideDrawer mSlidingMenu;
+
+    private ImageButton slidingButton;
+
+
+
+    public MainFragment()
+    {
+
+    }
+
+    public MainFragment(SimpleSideDrawer SlidingMenu)
+    {
+        mSlidingMenu = SlidingMenu;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        slidingButton = (ImageButton) rootView.findViewById(R.id.sliding_menu_button);
+        slidingButton.setOnClickListener(this);
+
+        return rootView;
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.sliding_menu_button:
+                mSlidingMenu.toggleDrawer();
+                break;
+            default:
+                break;
+        }
+    }
+
+
 }
