1b9faad7192ca6e95115ee446938b7cb7788c36a
Erickson <a820224@gmail.com>
2015-02-25 17:29:17 +0800

    Try to display Table list & sort out class file

--- app/src/main/java/net/erickson/yzucss_app/Fragments/UserTableViewFragment.java
+++ app/src/main/java/net/erickson/yzucss_app/Fragments/UserTableViewFragment.java
@@ -1,11 +1,25 @@
 package net.erickson.yzucss_app.Fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+
+import net.erickson.yzucss_app.Adapter.TablePageAdapter;
 
 /**
  * Created by Erickson on 2015/2/17.
  * View of user table
  */
 public class UserTableViewFragment extends Fragment {
+    private TablePageAdapter tablePageAdapter;
+    private ViewPager mViewPager;
 
+    @Override
+    public void onCreate(Bundle savedInstancedState)
+    {
+        super.onCreate(savedInstancedState);
+
+        tablePageAdapter = new TablePageAdapter(getFragmentManager(), getActivity());
+
+    }
 }
